be50c1f25942ba72544aa1e86dabeadcca1315b0
gaku yamamoto <githubgemini1124@gmail.com>
2021-02-16 12:49:22 +0900

    get iBeacon

--- app/src/main/java/com/example/altbeaconapp/MainActivity.kt
+++ app/src/main/java/com/example/altbeaconapp/MainActivity.kt
@@ -18,11 +18,12 @@ class MainActivity : AppCompatActivity(), BeaconConsumer {
     private var beaconManager: BeaconManager? = null
     // uuidの指定
     private val uuidString: String = "FFFE2D12-1E4B-0FA4-9F28-A317AE1848DE"
-//    private val major = Identifier.parse("6216")
-//    private val minor = Identifier.parse("56833")
+    private val major = Identifier.parse("6216")
+    private val minor = Identifier.parse("56833")
     private val uuid = Identifier.parse(uuidString)
     // ビーコンのフォーマット設定
     private val IBEACON_FORMAT: String = "m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"
+    private val region = Region("gaku-yamamoto-region", uuid,null,null)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,11 +36,11 @@ class MainActivity : AppCompatActivity(), BeaconConsumer {
         } else {
             showToast("このデバイスはBLE対応です", Toast.LENGTH_LONG)
         }
-
         if (Build.VERSION.SDK_INT >= 23) {
             checkPermission()
         }
         beaconManager = BeaconManager.getInstanceForApplication(this)
+        // BeaconParseを設定
         beaconManager!!.beaconParsers.add(BeaconParser().setBeaconLayout(IBEACON_FORMAT))
     }
 
@@ -59,109 +60,70 @@ class MainActivity : AppCompatActivity(), BeaconConsumer {
      * BeaconConsumer内のメソッドをoverride
      **************************************************/
     override fun onBeaconServiceConnect() {
-        try {
-            beaconManager!!.startMonitoringBeaconsInRegion(Region("AltBeaconMini",null,null,null))
-        } catch (e: RemoteException) {
-            Log.d("error",e.toString())
-            e.printStackTrace()
+        Log.d("","onBeaconServiceConnect")
+        // なんか知らないけど２重で登録されてしまうのでここで一旦削除
+        beaconManager?.removeAllMonitorNotifiers()
+        beaconManager?.removeAllRangeNotifiers()
+        beaconManager?.rangedRegions?.forEach {region ->
+            beaconManager?.stopRangingBeaconsInRegion(region)
         }
-        // モニタリングの通知受取り処理
-        beaconManager!!.addMonitorNotifier(object: MonitorNotifier{
-            // 領域内に侵入した時に呼ばれる
-            override fun didEnterRegion(region: Region) {
-                // レンジングの開始
-                beaconManager!!.startRangingBeaconsInRegion(region)
-                Log.d("didEnterRegion",region.toString())
-            }
-            // 領域外に退出した時に呼ばれる
-            override fun didExitRegion(region: Region) {
-                // レンジングの停止
-                beaconManager!!.stopRangingBeaconsInRegion(region)
-                Log.d("didExitRegion",region.toString())
-            }
-            // 領域への侵入/退出のステータスが変化した時に呼ばれる
-            override fun didDetermineStateForRegion(i: Int, region: Region) {
-                showToast(region.toString(), Toast.LENGTH_LONG)
-                Log.d("didDetermineState" +
-                        "",region.toString())
-            }
-        })
+        //BeaconManagerクラスのモニタリング設定
+        beaconManager?.addMonitorNotifier(monitorNotifier)
+        //BeaconManagerクラスのレンジング設定
+        beaconManager?.addRangeNotifier(rangeNotifier)
+        startMonitoringBeacons()
+    }
 
-        // レンジングの通知受け取り処理
-        beaconManager!!.addRangeNotifier { beacons, region ->
-
-            beacons
-                    .map { "UUID:" + it.id1 + " major:" + it.id2 + " minor:" + it.id3 + " RSSI:" + it.rssi + " Distance:" + it.distance + " txPower" + it.txPower }
-                    .forEach { Log.d("addRangeNotifier", it) }
-
-
-            // 範囲内のビーコン情報を受け取る
-            Log.d("region", region.toString())
-            Log.d("beacons", beacons.toString())
-
-            var maxMajor: Int?
-            var maxMinor: Int?
-
-            // 範囲内の複数のビーコン情報を保持させる変数
-            var getMajorList: ArrayList<Int> = ArrayList()
-            var getMinorList: ArrayList<Int> = ArrayList()
-            var getRssiList: ArrayList<Int> = ArrayList()
-
-            // 範囲内にビーコンがある時の処理
-            if (beacons.size > 0) {
-
-                // 範囲内のビーコンの数だけ繰り返す
-                for (beacon in beacons) {
-                    // 複数のビーコン情報をArrayListに分割
-                    getMajorList.add(beacon.id2.toInt())
-                    getMinorList.add(beacon.id3.toInt())
-                    getRssiList.add(beacon.rssi)
-                }
-
-                // RSSIが最も大きいインデックスを取得
-                var indexRssi: Int = getRssiList.indexOf(getRssiList.max())
-
-                // 取得したインデックスのmajor値・minor値を取得
-                maxMajor = getMajorList[indexRssi]
-                maxMinor = getMinorList[indexRssi]
-
-                // Viewの更新
-                //viewUpdate(maxMajor, maxMinor)
-            }
+    private fun startMonitoringBeacons(){
+        Log.d("","startMonitoringBeacons")
+        try {
+            beaconManager?.startMonitoringBeaconsInRegion(region)
+        } catch (e: RemoteException) {
+            e.printStackTrace()
         }
     }
 
-    /**************************************************
-     * メソッド
-     **************************************************/
+    private val monitorNotifier = object : MonitorNotifier {
+        override fun didEnterRegion(p0: Region?) {
+            Log.d("","didEnterRegion")
+            Log.d("",p0.toString())
+        }
+        override fun didExitRegion(p0: Region?) {
+            Log.d("","didExitRegion")
+        }
+        override fun didDetermineStateForRegion(p0: Int, p1: Region?) {
+            Log.d("","didDetermineStateForRegion")
+        }
+    }
+
+    private val rangeNotifier = RangeNotifier { beacons, region ->
+        Log.d("","rangeNotifier")
+        for (beacon in beacons) {
+            Log.d("",beacon.toString())
+        }
+    }
+
+
     // トースト表示のメソッド
     fun showToast(text: String, length: Int) {
-
         // トーストの生成と表示
         var toast: Toast = Toast.makeText(this, text, length)
         toast.show()
     }
-
     // パーミッションの許可チェック
     @RequiresApi(Build.VERSION_CODES.M)
     fun checkPermission() {
-
         // パーミッション未許可の時
         if (checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-
             // パーミッションの許可ダイアログの表示
             requestPermissions(arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION), 0)
         }
     }
-
     fun viewUpdate(major: Int?, minor: Int?) {
-
         // Viewの取得
         var majorTextView: TextView = findViewById(R.id.major) as TextView
         var minorTextView: TextView = findViewById(R.id.minor) as TextView
-
         majorTextView.text = "major:" + major
         minorTextView.text = "minor:" + minor
     }
-
 }
